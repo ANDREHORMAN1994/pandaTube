a431e3009375e29bdcca58e01ee4236ed1489d29
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2023-12-03 18:32:03 -0300

    :sparkles: add assets

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <Text style={{ fontFamily: 'Roboto_700Bold', color: '#fff' }} >HELLO</Text>
+      ) : (
+        <Text>LOADING...</Text>
+      )}
     </View>
   );
 }
@@ -14,7 +25,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#202024',
     alignItems: 'center',
     justifyContent: 'center',
   },
